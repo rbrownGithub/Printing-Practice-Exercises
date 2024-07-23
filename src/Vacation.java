759d6dc11fe4d5e24cec49af6a824174840dc802
rbrownGithub <rodalebrownstiegleredtech@outlook.com>
2024-07-23 01:14:51 -0400

    Create Vacation.java

---
+++ src/Vacation.java
@@ -0,0 +1,7 @@
+public class Vacation {
+    public static void main(String[] args) throws Exception {
+        System.out.println("I'd like to visit");
+        System.out.println("Tokyo");
+        System.out.println("Japan");
+    }
+}
