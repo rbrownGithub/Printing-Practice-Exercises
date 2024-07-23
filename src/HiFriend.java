777d36eabc4cc03d575019a2f717867c56bb0f8a
rbrownGithub <rodalebrownstiegleredtech@outlook.com>
2024-07-23 01:17:30 -0400

    Create HiFriend.java

---
+++ src/HiFriend.java
@@ -0,0 +1,6 @@
+public class HiFriend {
+    public static void main(String[] args) {
+        String friendName = "Bill Suh"; // Declare a String variable with a friend's name
+        System.out.println("Hi " + friendName + "!"); // Use string concatenation to join the greeting and the friend's name
+    }
+}
